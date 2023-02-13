2e4f6452c2e8eef9746f9ab5f074faeabdf60036
Muhammad Fakhrur Rizal <fakhrurrizal10@gmail.com>
2023-02-13 23:48:07 +0700

    deploy setup

--- app/src/config/ChatRequest.js
+++ app/src/config/ChatRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: 'http://localhost:5000'})
+const API = axios.create({baseURL: 'https://twittbook-socialmediaapp.up.railway.app'})
 
 export const userChats = (id) => API.get(`/chat/${id}`)
--- app/src/config/MessagesRequest.js
+++ app/src/config/MessagesRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: 'http://localhost:5000'})
+const API = axios.create({baseURL: 'https://twittbook-socialmediaapp.up.railway.app'})
 
 export const getMessages =(id)=> API.get(`/message/${id}`)
 export const addMessage = (data)=> API.post(`/message/`, data)
